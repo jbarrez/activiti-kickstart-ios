59b7394731c4fc666e0e511461d21c4c20f97a6c
Joram Barrez <joram.barrez@gmail.com>
2012-07-23 16:17:44 +0200

    Initial Commit

---
+++ Activiti Kickstart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Activiti Kickstart
+//
+//  Created by Joram Barrez on 23/07/12.
+//  Copyright (c) 2012 Alfresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Activiti Kickstart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Activiti Kickstart
+//
+//  Created by Joram Barrez on 23/07/12.
+//  Copyright (c) 2012 Alfresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
